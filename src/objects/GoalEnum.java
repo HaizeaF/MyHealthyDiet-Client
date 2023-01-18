6813af20df31e276ef2d22c6d62248ea6d06e214
JBar <101391346+STParadox@users.noreply.github.com>
2023-01-18 19:31:13 +0100

    Added: Diet Client RESTFUL, diet class and GoalEnum

---
+++ src/objects/GoalEnum.java
@@ -0,0 +1,27 @@
+package objects;
+
+/**
+ * Diet goal types: increase, decrease or maintain type.
+ * @author JulenB
+ */
+public enum GoalEnum {
+    /**
+     * Type of goal used by diets to know what the goal has the diet
+     * This type has the purpouse to increase weight.
+     */
+    INCREASE,
+    
+    
+    /**
+     * Type of goal used by diets to know what the goal has the diet
+     * This type has the purpouse to decrease weight.
+     */
+    DECREASE,
+    
+    
+    /**
+     * Type of goal used by diets to know what the goal has the diet
+     * This type has the purpouse to maintain weight.
+     */
+    MAINTAIN
+}
