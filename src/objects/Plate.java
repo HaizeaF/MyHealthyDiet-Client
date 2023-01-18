412271078ba148a7f0d1721b910775e80bfffeae
Haizea <haizea.franco@gmail.com>
2023-01-18 19:30:54 +0100

    Plate attributes changed to properties and PlateFacadeREST path modified.

--- src/objects/Plate.java
+++ src/objects/Plate.java
@@ -2,6 +2,12 @@ package objects;
 
 import java.io.Serializable;
 import java.util.List;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  * @author HaizeaF
@@ -14,162 +20,162 @@ public class Plate implements Serializable {
     /**
      * Identifier of the plate.
      */
-    private Integer plate_id;
+    private SimpleIntegerProperty plate_id;
 
     /**
      * Name of the plate.
      */
-    private String plateName;
+    private SimpleStringProperty plateName;
 
     /**
      * KiloCalories in the plate.
      */
-    private Float calories;
+    private SimpleFloatProperty calories;
 
     /**
      * Carbohydrates in the plate.
      */
-    private Float carbohydrates;
+    private SimpleFloatProperty carbohydrates;
 
     /**
      * Lipids in the plate.
      */
-    private Float lipids;
+    private SimpleFloatProperty lipids;
 
     /**
      * Proteins in the plate.
      */
-    private Float proteins;
+    private SimpleFloatProperty proteins;
 
     /**
      * What meal of the day is.
      */
-    private MealEnum mealType;
+    private SimpleObjectProperty<MealEnum> mealType;
     
     /**
      * Defines if the plate is vegetarian or not.
      */
 
-    private Boolean isVegetarian;
+    private SimpleBooleanProperty isVegetarian;
     
     /**
      * List of the ingredients the plate has.
      */
-    private List<Ingredient> ingredients;
+    private SimpleListProperty<Ingredient> ingredients;
     
     /**
      * List of the diets the plate is on.
      */
-    private List<Diet> diets;
+    private SimpleListProperty<Diet> diets;
     
     /**
      * Image of the plate.
      */
-    private byte[] plateImg;
+    private SimpleObjectProperty<byte[]> plateImg;
 
     public Plate(String plateName, Float calories, Float carbohydrates, Float lipids, Float proteins,
             MealEnum mealType, List<Ingredient> ingredients, Boolean isVegetarian, List<Diet> diets, byte[] plateImg) {
-        this.plateName = plateName;
-        this.calories = calories;
-        this.carbohydrates = carbohydrates;
-        this.lipids = lipids;
-        this.proteins = proteins;
-        this.mealType = mealType;
-        this.ingredients = ingredients;
-        this.isVegetarian = isVegetarian;
-        this.diets = diets;
-        this.plateImg = plateImg;
+        this.plateName = new SimpleStringProperty(plateName);
+        this.calories = new SimpleFloatProperty(calories);
+        this.carbohydrates = new SimpleFloatProperty(carbohydrates);
+        this.lipids = new SimpleFloatProperty(lipids);
+        this.proteins = new SimpleFloatProperty(proteins);
+        this.mealType = new SimpleObjectProperty<MealEnum>(mealType);
+        this.ingredients = new SimpleListProperty(ingredients);
+        this.isVegetarian = new SimpleBooleanProperty(isVegetarian);
+        this.diets = new SimpleListProperty(diets);
+        this.plateImg = new SimpleObjectProperty(plateImg);
     }
 
     public Plate() {
     }
 
     public void setPlate_id(Integer plate_id) {
-        this.plate_id = plate_id;
+        this.plate_id.set(plate_id);
     }
 
     public Integer getPlate_id() {
-        return plate_id;
+        return plate_id.get();
     }
 
     public void setPlateName(String plateName) {
-        this.plateName = plateName;
+        this.plateName.set(plateName);
     }
 
     public String getPlateName() {
-        return plateName;
+        return plateName.get();
     }
 
     public void setCalories(Float calories) {
-        this.calories = calories;
+        this.calories.set(calories);
     }
 
     public Float getCalories() {
-        return calories;
+        return calories.get();
     }
 
     public void setCarbohydrates(Float carbohydrates) {
-        this.carbohydrates = carbohydrates;
+        this.carbohydrates.set(carbohydrates);
     }
 
     public Float getCarbohydrates() {
-        return carbohydrates;
+        return carbohydrates.get();
     }
 
     public void setLipids(Float lipids) {
-        this.lipids = lipids;
+        this.lipids.set(lipids);
     }
 
     public Boolean getIsVegetarian() {
-        return isVegetarian;
+        return isVegetarian.get();
     }
 
     public void setIsVegetarian(Boolean isVegetarian) {
-        this.isVegetarian = isVegetarian;
+        this.isVegetarian.set(isVegetarian);
     }
 
     public List<Diet> getDiets() {
-        return diets;
+        return diets.get;
     }
 
     public void setDiets(List<Diet> diets) {
-        this.diets = diets;
+        this.diets.set(diets);
     }
 
     public Float getLipids() {
-        return lipids;
+        return lipids.get();
     }
 
     public void setProteins(Float proteins) {
-        this.proteins = proteins;
+        this.proteins.set(proteins);
     }
 
     public Float getProteins() {
-        return proteins;
+        return proteins.get();
     }
 
     public void setMealType(MealEnum mealType) {
-        this.mealType = mealType;
+        this.mealType.set(mealType);
     }
 
     public MealEnum getMealType() {
-        return mealType;
+        return mealType.get();
     }
 
     public void setIngredients(List<Ingredient> ingredients) {
-        this.ingredients = ingredients;
+        this.ingredients.set(ingredients);
     }
 
     public List<Ingredient> getIngredients() {
-        return ingredients;
+        return ingredients.get();
     }
 
     public byte[] getPlateImg() {
-        return plateImg;
+        return (byte[]) plateImg.get();
     }
 
     public void setPlateImg(byte[] plateImg) {
-        this.plateImg = plateImg;
+        this.plateImg.set(plateImg);
     }
     
     @Override
