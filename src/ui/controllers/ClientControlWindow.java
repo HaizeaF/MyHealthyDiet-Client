a6e02cee53f7452ef2d2c51284d5e0e4ef7bed8a
SendoaBadiola <sendoa.badiolabl@elorrieta-errekamari.com>
2023-01-20 10:20:40 +0100

    Continued with controller and fixed the .fxml

--- src/ui/controllers/ClientControlWindow.java
+++ src/ui/controllers/ClientControlWindow.java
@@ -6,8 +6,14 @@
 package ui.controllers;
 
 import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +24,54 @@ public class ClientControlWindow {
     
     private Stage stage;
     private static final Logger LOGGER = Logger.getLogger("ClientControlWindow.class");
+    @FXML
+    private Button buttonTips;
+    @FXML
+    private Button buttonPlates;
+    @FXML
+    private Button buttonDiets;
+    @FXML
+    private Button buttonIngredients;
+    @FXML
+    private Button buttonLogout;
+    @FXML
+    private Button buttonClients;
+    @FXML
+    private Button buttonReport;
+    @FXML
+    private Button buttonReport1;
+    @FXML
+    private Button buttonSearch;
+    @FXML
+    private Button buttonHelp;
+    @FXML
+    private MenuButton menuButtonFilters;
+    @FXML 
+    private MenuItem itemEnabled;
+    @FXML 
+    private MenuItem itemDisabled;
+    @FXML
+    private TableView tableClients;
+    @FXML
+    private TableColumn columnEmail;
+    @FXML
+    private TableColumn columnName;
+    @FXML
+    private TableColumn columnPasswordChange;
+    @FXML
+    private TableColumn columnLogin;
+    @FXML
+    private TableColumn columnPasswrd;
+    @FXML
+    private TableColumn columnStatus;
+    @FXML
+    private TableColumn columnAge;
+    @FXML
+    private TableColumn columnGenre;
+    @FXML
+    private TableColumn columnGoal;
+    @FXML
+    private TableColumn columnHeight;
     
     public Stage getStage() {
         return stage;
