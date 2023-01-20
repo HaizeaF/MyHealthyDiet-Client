ab27cd36c346730ab6934e1fa374405ca393c55d
SendoaBadiola <sendoa.badiolabl@elorrieta-errekamari.com>
2023-01-20 09:57:36 +0100

    started with the client controller

---
+++ src/myhealthydiet/ClientControlTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myhealthydiet;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import ui.controllers.ClientControlWindow;
+
+/**
+ *
+ * @author Sendoa
+ */
+public class ClientControlTest extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("ui/views/ClientAdminWindow.fxml"));
+        Parent root = (Parent) loader.load();
+        
+        ClientControlWindow controller = ((ClientControlWindow) loader.getController());
+        
+        controller.setStage(stage);
+        
+        controller.initStage(root);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
