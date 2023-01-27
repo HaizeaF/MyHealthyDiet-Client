4b53f9ffd9f9fe4c13e402eb091e748ce464b90f
SendoaBadiola <sendoa.badiolabl@elorrieta-errekamari.com>
2023-01-27 14:25:20 +0100

    fixed

--- src/objects/Client.java
+++ src/objects/Client.java
@@ -20,7 +20,7 @@ import javax.xml.bind.annotation.XmlTransient;
  *
  * @author Sendoa
  */
-@XmlRootElement
+@XmlRootElement(name="client")
 public class Client extends User{
 
     private static final long serialVersionUID = 1L;
